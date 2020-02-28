11192bd2fa2d4d0ea5022279c0df56732e0398eb
sai1004 <saikiran.bavandla@gmail.com>
2020-02-28 23:41:01 +0530

    commit

--- src/app/pages/Profile/profile-edit-page/profile-edit-page.component.ts
+++ src/app/pages/Profile/profile-edit-page/profile-edit-page.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit
+} from "@angular/core";
 
 @Component({
-  selector: 'app-profile-edit-page',
-  templateUrl: './profile-edit-page.component.html',
-  styleUrls: ['./profile-edit-page.component.css']
+  selector: "app-profile-edit-page",
+  templateUrl: "./profile-edit-page.component.html",
+  styleUrls: ["./profile-edit-page.component.css"]
 })
-export class ProfileEditPageComponent implements OnInit {
+export class ProfileEditPageComponent implements OnInit, AfterViewInit {
+  @ViewChild("spanEle") span: any;
 
-  constructor() { }
+  @ViewChild("para")
+  para: ElementRef;
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    console.log(this.span);
+    console.log("Values on ngAfterViewInit():");
+    console.log("Paragraph: ", this.para.nativeElement);
   }
-
 }
