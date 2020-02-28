187bbb40706b46b3f7c7a53d79427dbb882742d5
sai1004 <saikiran.bavandla@gmail.com>
2020-02-28 08:56:52 +0530

    commit

--- src/app/pages/Profile/profile-create-page/profile-create-page.component.ts
+++ src/app/pages/Profile/profile-create-page/profile-create-page.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from "@angular/core";
+import { Profile } from "src/app/entities/Profile";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 
 @Component({
-  selector: 'app-profile-create-page',
-  templateUrl: './profile-create-page.component.html',
-  styleUrls: ['./profile-create-page.component.css']
+  selector: "app-profile-create-page",
+  templateUrl: "./profile-create-page.component.html",
+  styleUrls: ["./profile-create-page.component.css"]
 })
 export class ProfileCreatePageComponent implements OnInit {
+  profile: Profile;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private dialogRef: MatDialogRef<ProfileCreatePageComponent>,
+    @Inject(MAT_DIALOG_DATA) data
+  ) {
+    this.profile = data.profile;
   }
 
+  ngOnInit(): void {}
+
+  save(profileForm: any) {
+    this.dialogRef.close(profileForm.value);
+  }
+
+  close() {
+    this.dialogRef.close();
+  }
 }
+
+// https://gist.github.com/jhades/a3770850f74893670f4610b6db655733#file-08-ts
--- src/app/pages/Profile/profile-view-page/profile-view-page.component.ts
+++ src/app/pages/Profile/profile-view-page/profile-view-page.component.ts
@@ -1,16 +1,18 @@
 import { Component, OnInit } from "@angular/core";
 import { Profile } from "../../../entities/Profile";
-import { from } from "rxjs";
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+  MatDialogConfig
+} from "@angular/material/dialog";
+import { ProfileCreatePageComponent } from "../profile-create-page/profile-create-page.component";
 @Component({
   selector: "app-profile-view-page",
   templateUrl: "./profile-view-page.component.html",
   styleUrls: ["./profile-view-page.component.css"]
 })
 export class ProfileViewPageComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-
   profile: Profile[] = [
     {
       id: "1",
@@ -39,4 +41,38 @@ export class ProfileViewPageComponent implements OnInit {
   ];
 
   displayedColumns: string[] = ["id", "name", "email", "mobile"];
+
+  constructor(public dialog: MatDialog) {}
+
+  ngOnInit(): void {}
+
+  openDialog(): void {
+    const dialogConfig = new MatDialogConfig();
+
+    // dialogConfig.position = {
+    //   top: "",
+    //   left: "40"
+    // };
+
+    dialogConfig.disableClose = true;
+    dialogConfig.autoFocus = true;
+
+    dialogConfig.data = {
+      profile: this.profile
+    };
+
+    this.dialog.open(ProfileCreatePageComponent, dialogConfig);
+
+    const dialogRef = this.dialog.open(
+      ProfileCreatePageComponent,
+      dialogConfig
+    );
+
+    dialogRef
+      .afterClosed()
+      .subscribe(data => console.log("Dialog output:", data)
+      
+      // this.profile = data
+      );
+  }
 }
